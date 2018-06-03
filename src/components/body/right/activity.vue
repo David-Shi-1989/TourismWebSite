577c9c651e936ff5452e1e3ea5ef59be9b96f893
David-Shi-1989 <David-Shi-1989@users.noreply.github.com>
2018-06-03 10:17:02 +0800

    增加活动倒计时

--- src/components/body/right/activity.vue
+++ src/components/body/right/activity.vue
@@ -9,20 +9,20 @@
       <div class="activity-imgWrap">
         <img src="/static/image/huodong/czx.jpg">
         <div class="activity-img-des">
-          <h5>108位冒险者，皖南川藏线等你来</h5>
-          <p>皖南川藏线又称“江南天路，位于皖南318”。该公路线东起宁国市的青龙乡，西至泾县的蔡村镇，其最神秘精华路段全长120公里。</p>
+          <h5>{{data.title}}</h5>
+          <p>{{data.subTitle}}</p>
         </div>
       </div>
       <div class="activity-desWrap">
-        <p class="text-indent-2">在这个夏天，我们召集108位热爱户外的驴友，一起来到风景绝美的皖南川藏线，一起自驾度过这个难忘的夏日。</p>
+        <p class="text-indent-2">{{data.content}}</p>
         <table class="activity-info-tb">
           <tr>
             <td style="width:90px;"><span class="ng-colon-after">所剩名额</span></td>
-            <td><span class="activity-highlight">38</span></td>
+            <td><span class="activity-highlight">{{data.leftCount}}</span></td>
           </tr>
           <tr>
             <td><span class="ng-colon-after">活动倒计时</span></td>
-            <td><span class="activity-highlight">56天14小时23分11秒</span></td>
+            <td><span class="activity-highlight">{{getCutdownTime}}</span></td>
           </tr>
         </table>
         <div style="text-align:center;margin-top:20px;">
@@ -37,9 +37,44 @@
 <script>
 export default {
   data () {
+    var now = (new Date()).valueOf()
     return {
-      data: []
+      data: {
+        id: '120sa2-21;',
+        title: '108位冒险者，皖南川藏线等你来',
+        subTitle: '皖南川藏线又称“江南天路，位于皖南318”。该公路线东起宁国市的青龙乡，西至泾县的蔡村镇，其最神秘精华路段全长120公里。',
+        content: '在这个夏天，我们召集108位热爱户外的驴友，一起来到风景绝美的皖南川藏线，一起自驾度过这个难忘的夏日。',
+        leftCount: 56,
+        endTime: 1533340800000,
+        nowTime: now
+      }
     }
+  },
+  computed: {
+    getCutdownTime () {
+      let result = {
+        day: 0,
+        hour: 0,
+        minute: 0,
+        second: 0
+      }
+      let dis = (this.data.endTime - this.data.nowTime)
+      let _s = 1000
+      let _m = _s * 60
+      let _h = _m * 60
+      let _d = _h * 24
+      result.day = Math.floor(dis / _d)
+      result.hour = Math.floor((dis - result.day * _d) / _h)
+      result.minute = Math.floor((dis - result.day * _d - result.hour * _h) / _m)
+      result.second = Math.floor((dis - result.day * _d - result.hour * _h - result.minute * _m) / _s)
+      return result.day + '天' + result.hour + '小时' + result.minute + '分' + result.second + '秒'
+    }
+  },
+  created () {
+    function timeTick () {
+      this.data.nowTime += 1000
+    }
+    setInterval(timeTick.bind(this), 1000)
   }
 }
 </script>
